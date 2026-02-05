41515ecf49e290e155b5d6b2851a6b04642ad7cf
Aditya Sinha <adityasinha1190@gmail.com>
2026-02-05 10:35:57 +0530

    Conditions

---
+++ 01-Practice Programs/Set-01/Conditions_004.java
@@ -0,0 +1,24 @@
+/*
+ * ProgramID : Calculator_003
+ * Author : Aditya Sinha
+ * Description : Determine the User is Adult
+ * Date : 22/1/26
+ * */
+
+import java.util.Scanner;
+
+class Conditions_004 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in); //Object Creation
+        System.out.print("Enter Your Age : ");//Handling User Input
+        int age = scn.nextInt();
+
+        //Conditions
+        if(age >= 18){
+            System.out.print("The User is Adult...");
+        }
+        else{
+            System.out.print("The User is not Adult...");
+        }
+    }
+}
