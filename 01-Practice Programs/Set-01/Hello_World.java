a67930f60c5b83153fa422f536a533c05ffe533d
Aditya Sinha <adityasinha1190@gmail.com>
2026-02-05 13:49:18 +0530

    Updates

--- 01-Practice Programs/Set-01/Hello_World.java
+++ 01-Practice Programs/Set-01/Hello_World.java
@@ -1,11 +1,11 @@
 /*
-* ProgramID : Hello_World_001
+* ProgramID : Hello_World
 * Author : Aditya Sinha
 * Description : First Java Program (Hello World)
 * Date : 22/10/25
 * */
 
-public class Hello_World_001 {
+public class Hello_World {
     public static void main(String[] args){
         System.out.println("Hello World"); //Final Output
     }
