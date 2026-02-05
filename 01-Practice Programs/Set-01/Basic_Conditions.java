3ad5344960a234f2d9449f6b793eab25fd8fef13
Aditya Sinha <adityasinha1190@gmail.com>
2026-02-05 13:47:07 +0530

    Updates

--- 01-Practice Programs/Set-01/Basic_Conditions.java
+++ 01-Practice Programs/Set-01/Basic_Conditions.java
@@ -1,5 +1,5 @@
 /*
- * ProgramID : Conditions_004
+ * ProgramID : Basic_Conditions
  * Author : Aditya Sinha
  * Description : Determine the User is Adult
  * Date : 22/1/26
@@ -7,7 +7,7 @@
 
 import java.util.Scanner;
 
-class Conditions_004 {
+public class Basic_Conditions {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in); //Object Creation
         System.out.print("Enter Your Age : ");//Handling User Input
