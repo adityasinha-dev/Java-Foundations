3e0ce9e775d738738ac2b6a50930f054310952c7
Aditya Sinha <adityasinha1190@gmail.com>
2026-02-05 10:38:37 +0530

    Even Odd

---
+++ 01-Practice Programs/Set-01/Even_Odd_005.java
@@ -0,0 +1,26 @@
+//
+/*
+ * ProgramID : Even_Odd_005
+ * Author : Aditya Sinha
+ * Description : Predict the Number is Odd or Even
+ * Date : 23/1/26
+ * */
+
+import java.util.Scanner;
+public class Even_Odd_005 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter Number to Check : ");
+        int num = scn.nextInt();
+
+        //Condition
+        if(num%2 == 0){
+            System.out.print("The Number is Even...");
+        }
+        else{
+            System.out.print("The Number is Odd..");
+        }
+
+    }
+}
